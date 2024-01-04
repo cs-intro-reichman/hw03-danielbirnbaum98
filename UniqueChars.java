f3426282b94dd9892b1950fdf7f5a295b7a233a4
Daniel <daniel.birnbaum@post.runi.ac.il>
2024-01-04 20:09:51 +0200

    NewUnique

--- UniqueChars.java
+++ UniqueChars.java
@@ -17,11 +17,16 @@ public class UniqueChars {
         for (int i = 0; i < length; i++)
         {
             c = s.charAt(i);
-            }
-            if (s_unique.indexOf(c) == -1 || c == ' ')
+            if (s_unique.indexOf(c) == -1)
             {
                 s_unique = s_unique + c;
             }
+            if (c == ' ')
+            {
+                s_unique = s_unique + c;
+            }
+        }
+            
             return s_unique;
         }
         
